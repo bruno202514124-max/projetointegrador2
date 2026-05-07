431a0c1a848861df7754b8cb83c52352fe09767c
LuanMarques1313 <luan.202413746@unilasalle.edu.br>
2026-05-07 13:26:25 -0300

    Revert "Atualizando tela de login"
    This reverts commit d3ab84454ff7156927e8cc2765c22753956139c4.

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,90 +1,18 @@
+import LayoutBase from '@/componentes/LayoutBase';
 import styles from '@/css/base.module.css';
 
-export default function Login() {
+export default function Home() {
   return (
-    <div className="container-fluid">
-
-      <div className="row vh-100">
-
-        {/* LADO ESQUERDO - IMAGEM */}
-        <div className="col-md-6 d-none d-md-block p-0">
-
-          <img
-            src="/img/bulldog-space.png"
-            alt="Bulldog Brewer"
-            style={{
-              width: '100%',
-              height: '100vh',
-              objectFit: 'cover'
-            }}
-          />
-
-        </div>
-
-        {/* LADO DIREITO - LOGIN */}
-        <div className="col-12 col-md-6 d-flex justify-content-center align-items-center">
-
-          <div
-            className={styles.cardBase}
-            style={{
-              maxWidth: '400px',
-              width: '100%'
-            }}
-          >
-
-            {/* LOGO */}
-            <div
-              className="text-center mb-0"
-              style={{ marginTop: '20px' }}
-            >
-              <img
-                src="/img/logo-sem-fundo.png"
-                alt="Bulldog Brewer"
-                style={{
-                  width: '140px',
-                  height: '140px',
-                  objectFit: 'contain'
-                }}
-              />
-            </div>
-
-            {/* TITULO */}
-           <h2
-              className={`${styles.sectionTitle} text-center`}
-              style={{ fontSize: '30px' }}
-            >
-              Login
-            </h2>
-
-            {/* INPUT ID */}
-            <div className="mb-3">
-              <input
-                type="text"
-                placeholder="Digite seu ID"
-                className="form-control"
-              />
-            </div>
-
-            {/* INPUT SENHA */}
-            <div className="mb-3">
-              <input
-                type="password"
-                placeholder="Digite sua senha"
-                className="form-control"
-              />
-            </div>
-
-            {/* BOTÃO */}
-            <button className="btn btn-warning w-100">
-              Entrar
-            </button>
-
-          </div>
-
-        </div>
-
+    <LayoutBase
+      titulo="Principal"
+      subtitulo="Base inicial do sistema web da Bulldog Brewer."
+    >
+      <div className={styles.cardBase}>
+        <h2 className={styles.sectionTitle}>Página inicial</h2>
+        <p className={styles.helpText}>
+          Esta é uma base simples para o projeto. Use o menu superior para acessar as páginas.
+        </p>
       </div>
-
-    </div>
+    </LayoutBase>
   );
 }
